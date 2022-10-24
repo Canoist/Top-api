e53e218c16e8bb7373fa3e363761e8ff7cf044a5
Leonid Lichkun <lionproklon@gmail.com>
2022-10-24 12:35:17 +0300

    Added some three modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ReviewModule } from './review/review.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TopPageModule, ReviewModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })

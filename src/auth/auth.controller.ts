c1d79b6d9fb69a0cdc6152f85a65e7e2c51b64c5
Leonid Lichkun <lionproklon@gmail.com>
2022-10-24 16:33:09 +0300

    Added controllers for every module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,12 @@
+import { AuthDto } from "./dto/auth.dto";
+import { Controller, Post, HttpCode, Body } from "@nestjs/common";
+
+@Controller("auth")
+export class AuthController {
+    @Post("register")
+    async register(@Body() dto: AuthDto) {}
+
+    @HttpCode(200)
+    @Post("login")
+    async login(@Body() dto: AuthDto) {}
+}
